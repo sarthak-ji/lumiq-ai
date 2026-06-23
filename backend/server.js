3562e9e42f6ef19c30bb843aa58c3a28ad78fa0a
Sarthak Srivastava <sesaralite01@gmail.com>
2026-06-23 23:44:03 +0530

    Setting up socket server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 import "./src/config/env.js";
 
+import http from "http";
 import app from './src/app.js';
 import connectDB from './src/config/database.js';
+import {initSocket} from './src/sockets/server.socket.js';
 
 
 const PORT = process.env.PORT || 8000;
+
+
+const httpServer = http.createServer(app);
+
+initSocket(httpServer);
 
 // Connect to database
 connectDB();
 
 // Start server
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
